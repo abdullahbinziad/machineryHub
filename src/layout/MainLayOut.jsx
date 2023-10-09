00af778f49399239a7564f6904d64784acf75292
Md. Abir mahmud <abirmahmud5665@gmail.com>
2023-10-09 14:54:31 +0600

    fixed footer

--- src/layout/MainLayOut.jsx
+++ src/layout/MainLayOut.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import Header from '../shared/header/Header';
-import Topbar from '../shared/topbar/Topbar';
-import Footer from '../shared/footer/Footer';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "../shared/header/Header";
+import Topbar from "../shared/topbar/Topbar";
+import Footer from "../shared/footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const MainLayOut = () => {
-    return (
-        <div className='bg-white'>
-         <div className=' relative bg-transparent  md:-mb-60 z-10'>
-         <Topbar></Topbar>
-            <Header></Header>
-            </div>  
-           <div className='bg-gray-200'>
-           <Outlet></Outlet>
-           </div>
-            <Footer></Footer>
-           
-        </div>
-    );
+  return (
+    <div className="bg-white">
+      <div className=" relative bg-transparent  md:-mb-60 z-10">
+        <Topbar></Topbar>
+        <Header></Header>
+      </div>
+      <div className="bg-gray-200">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayOut;
--- src/shared/header/MyNavbar.jsx
+++ src/shared/header/MyNavbar.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const MyNavbar = () => {
-    return (
-        <div>
-            This is navbar
-        </div>
-    );
+  return <div>This is navbar</div>;
 };
 
 export default MyNavbar;
